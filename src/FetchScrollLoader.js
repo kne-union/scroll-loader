4b904e08b7c1204019fc3d46e0a1e70d03418905
zhipenglin <zhipeng.lin@leapin.io>
2025-01-14 14:28:59 +0800

    修改分页参数覆盖关系错误

--- src/FetchScrollLoader.js
+++ src/FetchScrollLoader.js
@@ -3,90 +3,92 @@ import Fetch from '@kne/react-fetch';
 import ScrollLoader from './ScrollLoader';
 import get from 'lodash/get';
 import merge from 'lodash/merge';
-import { isNotEmpty } from '@kne/is-empty';
+import {isNotEmpty} from '@kne/is-empty';
 
 const FetchScrollLoader = props => {
-  const { dataFormat, mergeList, completeTips, api, searchProps, getSearchProps, children, ...others } = Object.assign(
-    {},
-    {
-      api: {
-        loader: () => {
-          return {
-            pageData: [],
-            totalCount: 0
-          };
-        }
-      },
-      mergeList: (data, newData) => {
-        return Object.assign({}, newData, {
-          pageData: data.pageData.concat(newData.pageData)
-        });
-      },
-      dataFormat: data => {
-        return {
-          list: data.pageData,
-          total: data.totalCount
-        };
-      },
-      children: props => {
-        console.log(props);
-        return null;
-      }
-    },
-    props
-  );
-
-  const pagination = Object.assign(
-    {},
-    {
-      paramsType: 'data',
-      current: 'currentPage',
-      pageSizeName: 'perPage',
-      pageSize: 20
-    },
-    props.pagination
-  );
-
-  const computedSearchProps = searchProps => {
-    if (getSearchProps && isNotEmpty(searchProps)) {
-      const requestProps = getSearchProps(searchProps);
-      return isNotEmpty(requestProps) ? { [pagination.paramsType]: requestProps } : {};
-    }
-    return {};
-  };
-
-  return (
-    <Fetch
-      {...Object.assign({}, api, computedSearchProps(searchProps))}
-      render={fetchApi => {
-        const formatData = dataFormat(fetchApi.data);
-        const current = get(fetchApi.requestParams, [pagination.paramsType, pagination.current], 1),
-          pageSize = get(fetchApi.requestParams, [pagination.paramsType, pagination.pageSizeName]) || pagination.pageSize;
-
-        const noMore = !formatData.total || current * pageSize >= formatData.total;
-        const onLoader = async () => {
-          await fetchApi.loadMore(
-            merge(
-              {
-                [pagination.paramsType]: {
-                  [pagination.pageSizeName]: pageSize,
-                  [pagination.current]: current + 1
+    const {dataFormat, mergeList, completeTips, api, searchProps, getSearchProps, children, ...others} = Object.assign(
+        {},
+        {
+            api: {
+                loader: () => {
+                    return {
+                        pageData: [],
+                        totalCount: 0
+                    };
                 }
-              },
-              computedSearchProps(searchProps)
-            ),
-            mergeList
-          );
-        };
+            },
+            mergeList: (data, newData) => {
+                return Object.assign({}, newData, {
+                    pageData: data.pageData.concat(newData.pageData)
+                });
+            },
+            dataFormat: data => {
+                return {
+                    list: data.pageData,
+                    total: data.totalCount
+                };
+            },
+            children: props => {
+                console.log(props);
+                return null;
+            }
+        },
+        props
+    );
 
-        return (
-          <ScrollLoader {...others} completeTips={completeTips} isLoading={!fetchApi.isComplete} noMore={noMore} onLoader={onLoader}>
-            {children({ fetchApi, list: formatData.list, data: fetchApi.data })}
-          </ScrollLoader>
-        );
-      }}
-    />
-  );
+    const pagination = Object.assign(
+        {},
+        {
+            paramsType: 'data',
+            current: 'currentPage',
+            pageSizeName: 'perPage',
+            pageSize: 20
+        },
+        props.pagination
+    );
+
+    const computedSearchProps = searchProps => {
+        if (getSearchProps && isNotEmpty(searchProps)) {
+            const requestProps = getSearchProps(searchProps);
+            return isNotEmpty(requestProps) ? {[pagination.paramsType]: requestProps} : {};
+        }
+        return {};
+    };
+
+    return (
+        <Fetch
+            {...Object.assign({}, api, computedSearchProps(searchProps))}
+            render={fetchApi => {
+                const formatData = dataFormat(fetchApi.data);
+                const current = get(fetchApi.requestParams, [pagination.paramsType, pagination.current], 1),
+                    pageSize = get(fetchApi.requestParams, [pagination.paramsType, pagination.pageSizeName]) || pagination.pageSize;
+
+                const noMore = !formatData.total || current * pageSize >= formatData.total;
+                const onLoader = async () => {
+                    await fetchApi.loadMore(
+                        merge(
+                            {},
+                            computedSearchProps(searchProps),
+                            {
+                                [pagination.paramsType]: {
+                                    [pagination.pageSizeName]: pageSize,
+                                    [pagination.current]: current + 1
+                                }
+                            }
+                        ),
+                        mergeList
+                    );
+                };
+
+                return (
+                    <ScrollLoader {...others} completeTips={completeTips} isLoading={!fetchApi.isComplete}
+                                  noMore={noMore} onLoader={onLoader}>
+                        {children({fetchApi, list: formatData.list, data: fetchApi.data})}
+                    </ScrollLoader>
+                );
+            }}
+        />
+    );
 };
 
 export default FetchScrollLoader;
